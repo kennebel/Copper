2e9f0211db8be9bdc28c82ec1172d004a408630f
Jeremy A. Cunningham <jac@kennebel.com>
2025-08-01 19:33:20 -0600

    Character moves, just don't let it collide!
    THe character moves around, but a collision causes unintended vectors to be added to movement.

--- scenes/things/Character/CharacterController.cs
+++ scenes/things/Character/CharacterController.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
 
-public partial class CharacterController : Node
+public partial class CharacterController : CharacterBody3D
 {
+	[ExportGroup("Movement")]
+	[Export]
+	public float Speed { get; set; } = 10f;
+
 	[ExportGroup("Shapes")]
 	[Export]
 	public MeshInstance3D MainBody { get; set; }
@@ -32,4 +36,45 @@ public partial class CharacterController : Node
 	public Node3D CrownRig { get; set; }
 	[Export]
 	public Node3D RearRig { get; set; }
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventKey keyEvent && keyEvent.IsActionPressed("move_forward"))
+		{
+			this.Velocity += Vector3.Forward * Speed;
+		}
+		else if (@event is InputEventKey keyEvent2 && keyEvent2.IsActionPressed("move_backward"))
+		{
+			this.Velocity += Vector3.Back * Speed;
+		}
+		else if (@event is InputEventKey keyEvent3 && keyEvent3.IsActionPressed("move_left"))
+		{
+			this.Velocity += Vector3.Left * Speed;
+		}
+		else if (@event is InputEventKey keyEvent4 && keyEvent4.IsActionPressed("move_right"))
+		{
+			this.Velocity += Vector3.Right * Speed;
+		}
+		else if (@event is InputEventKey keyEvent5 && keyEvent5.IsActionReleased("move_forward"))
+		{
+			this.Velocity -= Vector3.Forward * Speed;
+		}
+		else if (@event is InputEventKey keyEvent6 && keyEvent6.IsActionReleased("move_backward"))
+		{
+			this.Velocity -= Vector3.Back * Speed;
+		}
+		else if (@event is InputEventKey keyEvent7 && keyEvent7.IsActionReleased("move_left"))
+		{
+			this.Velocity -= Vector3.Left * Speed;
+		}
+		else if (@event is InputEventKey keyEvent8 && keyEvent8.IsActionReleased("move_right"))
+		{
+			this.Velocity -= Vector3.Right * Speed;
+		}
+    }
+
+	public override void _PhysicsProcess(double delta)
+	{
+		this.MoveAndSlide();
+	}
 }
